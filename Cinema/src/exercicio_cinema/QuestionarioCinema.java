c74319fa0f57ca94e9998cc3d1774903f6625cb2
SFStarke <sergiostarke@gmail.com>
2022-05-25 17:38:12 -0300

    Home01 25/05

--- Cinema/src/exercicio_cinema/QuestionarioCinema.java
+++ Cinema/src/exercicio_cinema/QuestionarioCinema.java
@@ -6,28 +6,88 @@ public class QuestionarioCinema {
 
 	public static void main(String[] args) {
 		Scanner read = new Scanner(System.in);
-		int capacidadeMax = 2, idade = 0, loop = 0;
-		int quantOtima = 0, quantBom=0, quantRegular=0, quantRuim;
-		int maiorIdadePessimo = 0, DifIdadeMaiorOtimoRuim = 0, maiorIdadeOtimo;
-		int maiorIdadeRuim, opiniao = 0;
-		float porcBomReg = 0, mediaIdadeRuim = 0,porcRespoPessimo = 0;
-		String otimo="*****", bom="****", regular="***", ruim="**", pessimo="*";
+		int capacidadeMax = 5, idade = 0, loop = 0;
+		int qtdOtimos = 0, qtdBom=0, qtdBomRegular = 0, qtdRegular=0, qtdRuim = 0, qtdPessimo = 0;
+		int maiorIdadePessimo = 0, DifIdadeMaiorOtimoRuim = 0, maiorIdadeOtimo = 0;
+		int maiorIdadeRuim = 0, opiniao = 0;
+		float percentBomReg = 0, mediaIdadeRuim = 0,percentRespPessimo = 0;
 		
-		for(int i = 1; i <= capacidadeMax; i++) {
-			while(idade <= 0) {
-				System.out.println("Idade Inválida.\nInforme sua Idade: Sempre maior que zero[ 0 ]");
-				idade = read.nextInt();
-			}
-			System.out.println("Selecione uma opinião:\n[5] ***** Ótimo:\n"
-					+ "[4] **** Bom:\n[3] *** Regular:\n[2] ** Ruim:\n[1] * Péssimo:");
-			while(opiniao < 1 || opiniao > 5) {
-				opiniao = read.nextInt();
-				System.out.println("Informe uma opinião válida:\n###########\n");
-				System.out.println("Selecione uma opinião:\n[5] ***** Ótimo:\n"
-						+ "[4] **** Bom:\n[3] *** Regular:\n[2] ** Ruim:\n[1] * Péssimo:");
-			}
+		for(int i = 1; i <=capacidadeMax; i++ ) {
 			
+			System.out.println ("INFORME SUA IDADE (VÁLIDA SEMPRE MAIOR QUE ZERO)");
+			idade = read.nextInt();
+			
+			while(idade <= 0 ) {
+            	System.out.println ("VALOR INVÁLIDA. INFORME NOVAMENTE SUA IDADE (SEMPRE MAIOR QUE ZERO)");
+            	idade = read.nextInt();
+            }
+			
+			System.out.println("Selecione sua opinião!");
+            System.out.println("5 - ***** Ótimo!");
+            System.out.println("4 - **** Bom!");
+            System.out.println("3 - *** Regular!");
+            System.out.println("2 - ** Ruim!");
+            System.out.println("1 - * Péssimo!");
+            opiniao = read.nextInt();
+            while( opiniao < 1 || opiniao > 5 ) {
+            	System.out.println ("INFORME UMA OPINIAO VÁLIDA. ");
+            	System.out.println("Selecione sua opinião!");
+	            System.out.println("5 - ***** Ótimo!");
+	            System.out.println("4 - **** Bom!");
+	            System.out.println("3 - *** Regular!");
+	            System.out.println("2 - ** Ruim!");
+	            System.out.println("1 - * Péssimo!");
+            	opiniao = read.nextInt();
+            }
+            
+            switch(opiniao) {
+            case 5:
+            	qtdOtimos++;
+            	if(idade > maiorIdadeOtimo) {
+            		maiorIdadeOtimo = idade;
+            	}	
+            	//System.out.println (qtdOtimos);
+            	break;
+            case 4:
+            	qtdBom++;
+            	qtdBomRegular++;
+            	//System.out.println (qtdBom);
+            	break;
+            case 3:
+            	qtdRegular++;
+            	qtdBomRegular++;
+            	//System.out.println (qtdRegular);
+            	break;
+            case 2:
+            	qtdRuim++;
+            	mediaIdadeRuim += idade;
+            	if(idade > maiorIdadeRuim) {
+            		maiorIdadeRuim = idade;
+            	}	
+            	//System.out.println (qtdRuim);
+            	break;
+            case 1:
+            	qtdPessimo++;
+            	//System.out.println (qtdPessimo);
+            	if(idade > maiorIdadePessimo) {
+            		maiorIdadePessimo = idade;
+            	}	
+            	break;
+            default:
+            }
+
 		}
+		
+		 percentBomReg = qtdBomRegular/(qtdBom - qtdRegular);
+		 mediaIdadeRuim /= qtdRuim;
+		 //mediaIdadeRuim = mediaIdadeRuim/qtdRuim;
+		 percentRespPessimo =  (qtdPessimo*100)/capacidadeMax;
+		 DifIdadeMaiorOtimoRuim = maiorIdadeOtimo - maiorIdadeRuim; 
+		 System.out.println ("A quantidade de respostas Ótimo: " + qtdOtimos);
+     	 System.out.println("A diferença percentual entre respostas bom e regular: " +percentBomReg);
+         System.out.println("A média de idade das pessoas que responderam ruim: "+ mediaIdadeRuim);
+         System.out.println("A percentagem de respostas péssimo é de "+percentRespPessimo+" . A maior idade que utilizou esta opinião: "+ maiorIdadePessimo );
+         System.out.println("A diferença de idade entre a maior idade que respondeu ótimo e a maior idade que respondeu ruim: " + DifIdadeMaiorOtimoRuim );
 
 		read.close();
 	}
