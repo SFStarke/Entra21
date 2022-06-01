7c78340de9c2dd52acb067ae74785b1e64218d32
SFStarke <sergiostaker@gmail.com>
2022-06-01 17:53:45 -0300

    Home-02 01/06

--- ExerciciosExtra/src/ListaExtraSeis/Quest01.java
+++ ExerciciosExtra/src/ListaExtraSeis/Quest01.java
@@ -3,31 +3,45 @@ package ListaExtraSeis;
 import java.util.Scanner;
 
 public class Quest01 {
-/*Questao 1 - Escreva um programa que verifique a validade de uma senha 
-fornecida pelo usuário. A senha válida é o número 1234. Se a senha 
-informada pelo usuário for inválida, a mensagem ACESSO NEGADO deve ser 
-impressa e repetida a solicitação de uma nova senha até que ela seja válida. 
-Caso contrário, deve ser impressa a mensagem ACESSO PERMITIDO junto com 
-um número que representa quantas vezes a senha foi informada.
- * */
+	/*
+	 * Questao 1 - Escreva um programa que verifique a validade de uma senha
+	 * fornecida pelo usuário. A senha válida é o número 1234. Se a senha informada
+	 * pelo usuário for inválida, a mensagem ACESSO NEGADO deve ser impressa e
+	 * repetida a solicitação de uma nova senha até que ela seja válida. Caso
+	 * contrário, deve ser impressa a mensagem ACESSO PERMITIDO junto com um número
+	 * que representa quantas vezes a senha foi informada.
+	 */
 	public static void main(String[] args) {
-		int senha[] = {1,2,3,4};
-		
+		int senha[] = { 1234 };
+		int numero[] = new int[senha.length];
+		String res = "";
+		int vezesDigitado = 1;
 		boolean confirma = false;
 		Scanner read = new Scanner(System.in);
-		
-		System.out.println("Senha: ");
+
 		do {
+			System.out.print("Senha: ");
+			for (int i = 0; i < numero.length; i++) {
+				numero[i] = read.nextInt();
+			}
 			for (int i = 0; i < senha.length; i++) {
-				System.out.print("");
-				senha[i] = read.nextInt();
+				if (senha[i] == numero[i]) {
+					confirma = true;
+				} else {
+					confirma = false;
+				}
+				if (confirma == true) {
+					System.out.println("\n###############\nACESSO PERMITIDO");
+				} else {
+					System.out.println("\n###############\nACESSO NEGADO:\nRefaça sua senha.\n###############\n");
+					vezesDigitado++;
+				}
 			}
-			if(senha[i]== ){
-				
-			}
-				
-		}while(confirma == false);
-		
+			System.out.println(res);
+		} while (confirma == false);
+		res = (vezesDigitado == 1) ? "Foi realizada "+vezesDigitado+" tentativa para o acesso." :
+			"Foram realizadas "+vezesDigitado+" tentativas para o acesso.";
+		System.out.println(res);
 		read.close();
 	}
 }
---
+++ ExerciciosExtra/src/ListaExtraSeis/Quest02.java
@@ -0,0 +1,17 @@
+package ListaExtraSeis;
+
+import java.util.Scanner;
+
+public class Quest02 {
+	/*
+	 * Questão 2 - Escreva um programa que determine se dois valores inteiros e
+	 * positivos A e B são primos entre si (dois números inteiros são ditos primos
+	 * entre si caso não exista divisor comum aos dois números, com exceção do valor
+	 * 1).
+	 */
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		in.close();
+	}
+}
