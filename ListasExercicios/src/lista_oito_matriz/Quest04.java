fb0e0ff37a7765c7a96d0b8e5c4df5728a00ba3d
SFStarke <sergiostaker@gmail.com>
2022-06-07 18:15:47 -0300

    Home-01 07/06

--- ListasExercicios/src/lista_oito_matriz/Quest04.java
+++ ListasExercicios/src/lista_oito_matriz/Quest04.java
@@ -15,37 +15,82 @@ armazenado. Dica. Usar estrutura de repetição para escolha no
 Menu.
 */
 public class Quest04 {
-public static void main(String[] args) {
-	String [][] agenda = new String [31][24];
-	Scanner read = new Scanner(System.in);
-	boolean res = true;
-	int dia = 0, hora = 0;
-	String com = "";
-	
-	while(res == true) {
-		System.out.print("Qual é o dia? ");
-		dia = read.nextInt();
-		System.out.print("Qual é a hora? ");
-		hora = read.nextInt();
-		System.out.print("Qual é o compromisso? ");
-		com = read.next();
-		agenda[dia][hora] = com;
-		System.out.print("Deseja continuar? Sim[s] Não[n]");
-		res = read.nextBoolean();
-		
-	}
-	
-	for (int i = 0; i < agenda.length; i++) {
-		System.out.println();
-		for (int j = 0; j < agenda[i].length; j++) {
-			if(agenda[i][j] == null) {
-				continue;
+	public static void main(String[] args) {
+		String[][] agenda = new String[31][24];
+		Scanner read = new Scanner(System.in);
+		String resAdd = "s", res = "";
+		String com = "";
+		boolean loop = true;
+
+		System.out.println("Agenda de compromissos:\n" +
+		"###################################\n");
+		do {
+			System.out.println("\n**************************************\n"
+					+ "O que deseja fazer?\n" +
+			"Adicionar compromisso [a], Consultar Compromissos [c] ou Encerrar [e]");
+			res = read.next();
+			if(res.equalsIgnoreCase("a") ^ res.equalsIgnoreCase("c") ^ res.equalsIgnoreCase("e") ) {
+				
+				if (res.equalsIgnoreCase("a")) {
+					do {
+						int dia = 0, hora = 0;
+						System.out.print("Qual é o dia? ");
+						dia = read.nextInt();
+						System.out.print("Qual é a hora? ");
+						hora = read.nextInt();
+						com = read.nextLine();
+						System.out.print("Qual é o compromisso? ");
+						com = read.nextLine();
+						
+						agenda[--dia][--hora] = com;
+						System.out.print("\n¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨\n"
+								+ "Agendar Novo compromisso? Sim[s] Não[n]");
+						
+						resAdd = read.next();
+					} while (resAdd.equalsIgnoreCase("s"));
+				} else if(res.equalsIgnoreCase("c")){
+					
+					do {
+					System.out.println("\n¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨\n"
+							+ "Compromissos agendados para os dias:");
+					for (int i = 0; i < agenda.length; i++) {
+						for (int j = 0; j < agenda[i].length; j++) {
+							if (agenda[i][j] == null) {
+								continue;
+							} else {
+								int dia = i, hora = j;
+								System.out.printf("\nDia: %d. Hora: %d.",++dia,++hora);
+							}
+						}
+					}
+					System.out.print("\n¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨\n"
+							+ "Para consultar o seu compromisso, siga as instruções:\n"
+							+ "Digite o dia: ");
+					int dia = read.nextInt();
+					System.out.print("Digite a hora: ");
+					int hora = read.nextInt();
+					if(agenda[--dia][--hora] != null) {
+						System.out.println("\nSeu compromisso do dia é:\n"+agenda[dia][hora]);
+					}else {
+						System.out.println("\nSEM COMPROMISSO AGENDADO...");
+					}
+					System.out.print("\n¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨\n"
+							+ "Consultar novo compromisso? Sim[s] Não[n]");
+					resAdd = read.next();
+				} while (resAdd.equalsIgnoreCase("s"));
+					
+				}else if(res.equalsIgnoreCase("e")) {
+					break;
+				}
 			}else {
-				System.out.println(agenda[i][j]);
+				System.out.println("\n**********************************\n"
+						+ "Comando errado...\nAceitos somente [a], [c] ou [e]:\n"
+						+ "**************************************\n");
+				continue;
 			}
-		}
+
+		} while (loop == true);
+
+		read.close();
 	}
-	
-	read.close();
-}
 }
