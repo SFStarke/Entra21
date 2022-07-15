c8d0830bc8631563f2f1f26a02ff9b5b0709f1fb
sergiostarke@gmail.com <sergiostarke@gmail.com>
2022-07-15 11:58:49 -0300

    Class01- 15/07

--- ListasExercicios/src/oop_lista_cinco/MainContaCancaria.java
+++ ListasExercicios/src/oop_lista_cinco/MainContaCancaria.java
@@ -1,30 +1,48 @@
 package oop_lista_cinco;
 
+import java.util.Scanner;
+
 //import java.util.Calendar;
 
 public class MainContaCancaria {
 
 	public static void main(String[] args) {
 		//Calendar d = Calendar.getInstance();
-		ContaPoupanca cp = new ContaPoupanca("Sérgio", 1, 500, 14);//String nomeCliente, int numConta, double saldo, String diaRendimento
-		ContaEspecial ce = new ContaEspecial("Felipe",2, 500, 1000); // String nomeCliente, int numConta, double saldo, double limite
+		ContaPoupanca poupanca = new ContaPoupanca();
+		ContaEspecial especial = new ContaEspecial();
+		Scanner read = new Scanner(System.in);
+		boolean loop = true;
+		String resLoop;
+		do {
+			
+			do {
+				System.out.println("Para Continuar [ Enter ] | Para Sair [ S + Enter ]");
+				resLoop = read.next();
+				
+			}while();//FALTA TERMINAR AQUI....... POR ISSO O BUG............
+			loop = resLoop.equalsIgnoreCase("s") ? false: true;
+		}while(loop ==true);
 		
+		read.close();
+	}
+	/*
+		ContaPoupanca cp = new ContaPoupanca("Sérgio", 1, 500, 10);//String nomeCliente, int numConta, double saldo, String diaRendimento
+		ContaEspecial ce = new ContaEspecial("Felipe",2, 500, 1000); // String nomeCliente, int numConta, double saldo, double limite
 		
 		System.out.println("\nCONTA POUPANÇA:\n");
 		cp.depositar(10);
-		cp.calcularNovoSaldo();
 		cp.sacar(511);
+		cp.calcularNovoSaldo();
 		System.out.println(cp.toString());
 		
 		System.out.println("CONTA ESPECIAL:\n");
-	//	ce.depositar(0);
+		ce.depositar(0);
 		ce.sacar(600);
-		ce.sacar(50);
-		ce.depositar(145);
+		ce.sacar(600);
+		ce.depositar(600);
 		ce.sacar(5);
 		ce.depositar(510);
-	//	ce.sacar(0);
+		ce.sacar(0);
 		System.out.println(ce.toString());
-		
-	}
+	 */
 }
