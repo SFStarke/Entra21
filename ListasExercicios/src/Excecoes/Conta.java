35b2b51f6c175bc64584b1619aaadb4385ca32f0
SFStarke <sergiostarke@gmail.com>
2022-07-21 16:40:03 -0300

    Home01 21/07

---
+++ ListasExercicios/src/Excecoes/Conta.java
@@ -0,0 +1,10 @@
+package Excecoes;
+
+public class Conta {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
---
+++ ListasExercicios/src/Excecoes/ContaExecao.java
@@ -0,0 +1,6 @@
+package Excecoes;
+
+public class ContaExecao extends Exception{
+
+	  
+}
